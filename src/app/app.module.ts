057dd709b76499d1922ec8750e277157a8dac69c
Rade Milovic <r.milovic90@gmail.com>
2019-06-10 22:45:39 +0200

    Add built-in validation to the form.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,4 +11,8 @@ import { AppComponent } from "./app.component";
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+  name: string;
+  email: string;
+  password: string;
+}
